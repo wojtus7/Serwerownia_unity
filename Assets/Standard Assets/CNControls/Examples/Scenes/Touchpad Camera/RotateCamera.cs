9c2e09d84288f365deb761f5f8270d1c437196f2
slawomirpolak <slawomirpolak93@gmail.com>
2017-02-12 18:28:58 +0100

    Added meterials

---
+++ Assets/Standard Assets/CNControls/Examples/Scenes/Touchpad Camera/RotateCamera.cs
@@ -0,0 +1,19 @@
+﻿using CnControls;
+using UnityEngine;
+
+namespace Examples.Scenes.TouchpadCamera
+{
+    public class RotateCamera : MonoBehaviour
+    {
+        public float RotationSpeed = 15f;
+        public Transform OriginTransform;
+
+        public void Update()
+        {
+            var horizontalMovement = CnInputManager.GetAxis("Horizontal");
+			var verticalMovement = CnInputManager.GetAxis("Vertical");
+			OriginTransform.Rotate(Vector3.up, horizontalMovement * Time.deltaTime * RotationSpeed);
+			OriginTransform.Rotate(Vector3.right, verticalMovement * Time.deltaTime * RotationSpeed);
+        }
+    }
+}
