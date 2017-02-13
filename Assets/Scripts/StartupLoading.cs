588f2a5d2f87aaf206bd82e0313a6d63e81133b8
jdi4 <jzxp.a4t@gmail.com>
2017-02-13 03:46:27 +0100

    Positioning almost ok

--- Assets/Scripts/StartupLoading.cs
+++ Assets/Scripts/StartupLoading.cs
@@ -8,8 +8,9 @@ using UnityEngine;
 public class StartupLoading : MonoBehaviour {
 
     string prefabName = "brak";
-    GameObject lastItem;
-    public Vector3 startingOffset = new Vector3 { x = -4.062f, y = -2.6f, z = 2.512f };
+    GameObject newItem;
+    public Vector3 startingOffset = new Vector3 { x = -4.062f, y = -0.9f, z = 2.512f };
+    public float verticalSpacing = 0.05f;
 
     struct Item
     {
@@ -34,31 +35,46 @@ public class StartupLoading : MonoBehaviour {
         {
             var items = cabinet.Elements();
             var cabinetObject = GameObject.Find(cabinet.Attribute("name").Value);
-            float newOffset = 0.0f;
+            float heightOffset = 0.0f;
 
             foreach (var item in items)
             {
                 prefabName = item.Value;
-                var newPrefab = Resources.Load(prefabName);
+                var prefabType = Resources.Load(prefabName);
 
                 var newPosition = startingOffset;
 
-                if (newPrefab != null)
+                if (prefabType != null)
                 {
                     //newPosition = cabinetObject.transform.position;
                     //newPosition.x -= cabinetObject.transform.lossyScale.x;
                     //newPosition.z -= cabinetObject.transform.lossyScale.z;
 
-                    lastItem = Instantiate(newPrefab, cabinetObject.transform) as GameObject;
+                    newItem = Instantiate(prefabType, cabinetObject.transform) as GameObject;
 
-                    float height = lastItem.GetComponent<Renderer>().bounds.size.y;
-                    newOffset += height; //* 2.0f;
+                    float height = newItem.GetComponent<Renderer>().bounds.extents.z;
+                    heightOffset += height; //* 2.0f;
+                    var debugext = newItem.GetComponent<Renderer>().bounds.extents;
+                    Debug.Log(String.Format("{0} > Extens x: {1}; Extens y: {2}; Extens z: {3}"
+                        , prefabName, debugext.x, debugext.y, debugext.z));
 
                     //newPosition.y -= cabinetObject.GetComponent<Renderer>().bounds.center.y;
-                    newPosition.y += newOffset;
+                    newPosition.y += heightOffset;
 
-                    lastItem.transform.position = newPosition;
-                    lastItem.transform.Rotate(90, 0, 0);
+                    newItem.transform.position = newPosition;
+
+                    if(item.Element("rotation") != null)
+                    {
+                        Vector3 r;
+                        r.x = float.Parse(item.Element("rotation").Attribute("x").Value);
+                        r.y = float.Parse(item.Element("rotation").Attribute("y").Value);
+                        r.z = float.Parse(item.Element("rotation").Attribute("z").Value);
+                        newItem.transform.Rotate(r);
+                    }
+                    else
+                        newItem.transform.Rotate(90, 0, 0);
+
+                    heightOffset += height + verticalSpacing;
 
                 }
             }
